9737d932f372561651a545680312b5dd1b601c9d
AlbertoPachecoDev <alberto.pg@chihuahua.tecnm.mx>
2021-09-03 21:36:25 -0600

    Ver. 0.5 3 Tweens, flags & delete
    Going for a gamble game.

--- godot.gd
+++ godot.gd
@@ -2,6 +2,8 @@ extends Sprite
 
 var vel
 var spin
+var bouncex = false
+var bouncey = false
 onready var screensize = get_viewport_rect().size
 onready var width = texture.get_width() / 2
 
@@ -10,7 +12,9 @@ func _ready():
 		null, 0.8, 2, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$efectoy.interpolate_property(self, 'scale:y',
 		null, 0.8, 2, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	vel = Vector2(rand_range(70,220),0).rotated(rand_range(0.4, PI/2))
+	$efecto_final.interpolate_property(self, 'modulate:a',
+		null, 0, 0.4, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	vel = Vector2(rand_range(90,220),rand_range(50,100)).rotated(rand_range(0.4, PI/2))
 	spin = rand_range(-PI, PI)
 	set_position(Vector2(rand_range(150,screensize.x-150), 100))
 	set_process(true)
@@ -24,12 +28,19 @@ func _process(delta):
 		Score.update_score(10)
 		$efectox.start()
 		if not $bounce.playing: $bounce.play()
+		if not bouncex:
+			bouncex = true
+			if bouncey: $efecto_final.start()	
 	if pos.y+width >= screensize.y or pos.y-width <= 0:
 		vel.y *= -1
 		Score.update_score(10)
 		$efectoy.start()
 		if not $bounce.playing: $bounce.play()
+		if not bouncey:
+			bouncey = true
+			if bouncex: $efecto_final.start()
 	set_position(pos)
 
-#func _on_efecto_tween_completed(object, key):
-#	add new tween + interpolate
+func _on_efecto_final_tween_completed(object, key):
+	if bouncex and bouncey:
+		queue_free()
