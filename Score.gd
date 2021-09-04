5a3f20faae25002aaf3c5a71fc32c899f9e3fc03
AlbertoPachecoDev <alberto.pg@chihuahua.tecnm.mx>
2021-09-04 12:55:11 -0600

    Ver. 0.8 Timers & signals
    Score.gd: Timing control via Timers & Signals
    Tie mechanism implemented if timer2 waits longer (nwait)

--- Score.gd
+++ Score.gd
@@ -8,6 +8,7 @@ var level = 1
 var score = 0
 var left = 0
 var gamer = 0
+var nwait = 0
 
 const Digits = [
 	 preload("res://images/0.png"),
@@ -31,20 +32,48 @@ func _ready():
 	randomize()
 	gamer = 5
 	z_index = 1
+	$wait1.start()
+	$wait2.stop()
 		
 func update_score(id):
-	if id == gamer: score_replay()
+	if id == gamer:
+		score_replay()
+		return
 	left += 1
-	if 9==left: score_replay()
+	if 9==left:
+		score_replay()
+		return
+
+func score_and_reset(points, tie=false):
+	score = round((score + points) / 2.0)
+	$score_label.text = "Score: " + str(score)
+	left = 0
+	nwait = 0
+	$wait1.stop()
+	$wait2.stop()
+	$end.pitch_scale = 1.5 if tie else 1.0 # empate
+	$end.play()
 		
 func score_replay():
 	var points = Points[left]
-	if 0==score: score=points 
-	score = round((score + points) / 2.0)
-	$score_label.text = "Score: " + str(score)
-	$end.play()
+	if 0==score: score=points
+	score_and_reset(points)
 
 func _on_end_finished():
-	left = 0
+	$wait1.start()
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Main.tscn")
+
+func _on_wait1_timeout():
+	$wait2.start()
+	print("end-timer1")
+
+func _on_wait2_timeout():
+	nwait += 1
+	print("end-timer2:", nwait)
+	match left:
+		9: score_and_reset(100)
+		8: if nwait>1: score_and_reset(95, true)
+		7: if nwait>2: score_and_reset(90, true)
+		6: if nwait>3: score_and_reset(80, true)
+		_: if nwait>4: score_and_reset(70, true)
